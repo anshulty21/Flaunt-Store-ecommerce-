674e721ef433e7608926097a394fc4ff088131c9
Anshul Tyagi <53966236+anshulty21@users.noreply.github.com>
2022-07-19 20:04:10 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 const { Cookie } = require("express-session");
 const { session } = require("passport");
 
-dbPassword = 'mongodb://127.0.0.1:27017/myproject?retryWrites=true';
+dbPassword = 'mongodb+srv://flstore:zJpNMCvPGL1c7aZw@cluster0.z6mpi4v.mongodb.net/?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword
